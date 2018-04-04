ec3f20207f17f9b2903a8f0d305b938f3ca180dd
Vrabel, Zdenko <zdenko.vrabel@wirecard.com>
2018-04-04 12:07:32 +0200

    added yaml config.

--- manifests/config.pp
+++ manifests/config.pp
@@ -11,6 +11,7 @@
 # Copyright 2018 Zdenko Vrabel
 #
 class linkerd::config(
+  $home  = $::linkerd::home,
   $group = $::linkerd::group,
   $user  = $::linkerd::user
 ) {
@@ -25,4 +26,15 @@ class linkerd::config(
     content => template("$module_name/linkerd.environment.erb")
   }
 
+  yaml_setting { 'linkerd::admin::ip':
+    target => "$home/config/linkerd.yaml",
+    key    => 'admin/ip',
+    value  => '0.0.0.0'
+  }
+
+  yaml_setting { 'linkerd::admin::port':
+    target => "$home/config/linkerd.yaml",
+    key    => 'admin/port',
+    value  => '9999'
+  }
 }
