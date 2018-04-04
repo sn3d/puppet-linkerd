9700c388568f7f419a86d170c9d7fe4ff9f3ed79
Vrabel, Zdenko <zdenko.vrabel@wirecard.com>
2018-04-04 18:10:42 +0200

    added uri prefix to marathon

--- manifests/namer/marathon.pp
+++ manifests/namer/marathon.pp
@@ -1,7 +1,8 @@
 class linkerd::namer::fs(
   $config_file = "$::linkerd::home/config/linkerd.yaml",
   $host = undef,
-  $port = 8080
+  $port = 8080,
+  $uri_prefix = "/marathon"
 ) {
   concat::fragment{ "linkerd_fragment_namer_marathon":
     target  => $config_file,
