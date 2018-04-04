30989830b12208a665b917ffa118c6d400004c28
Vrabel, Zdenko <zdenko.vrabel@wirecard.com>
2018-04-04 17:12:52 +0200

    fixed typo

--- manifests/router.pp
+++ manifests/router.pp
@@ -8,7 +8,7 @@ define linkerd::router(
     $ip       = "0.0.0.0"
 ) {
   concat::fragment{ "linkerd_fragment_router_$name":
-    target  => "$home/config/linkerd.yaml",
+    target  => $config_file,
     content => template("$module_name/linkerd.router.erb"),
     order   => 200 + $priority
   }
